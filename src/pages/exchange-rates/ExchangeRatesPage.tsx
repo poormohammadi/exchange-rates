ab38a00ecd3777b8d2ea6cd99f83d5656d0f2297
Ali Poormohammadi <poormohammmadi@gmail.com>
2025-11-18 18:55:45 +0000

    feat: integrate components and redux data

--- src/pages/exchange-rates/ExchangeRatesPage.tsx
+++ src/pages/exchange-rates/ExchangeRatesPage.tsx
@@ -1,9 +1,45 @@
-import { Container, Typography } from '@mui/material';
-import React, { useState } from 'react';
+import { Box, Container, Typography } from '@mui/material';
+import React, { useEffect } from 'react';
+
+import {
+  addCurrency,
+  loadAvailableCurrencies,
+  removeCurrency,
+  setBaseCurrency,
+  setSelectedDate,
+  useAppDispatch,
+  useAppSelector,
+} from '../../store';
+import { CurrencySelector } from './CurrencySelector';
+import { CurrencyTable } from './CurrencyTable';
 import { DatePicker } from './DatePicker';
 
 export const ExchangeRatesPage = () => {
-  const [selectedDate, setSelectedDate] = useState<string>('');
+  const dispatch = useAppDispatch();
+  const {
+    baseCurrency,
+    selectedCurrencies,
+    availableCurrencies,
+    selectedDate,
+    exchangeRates,
+    loading,
+  } = useAppSelector(state => state.currency);
+
+  const handleAddCurrency = (currency: string) => {
+    dispatch(addCurrency(currency));
+  };
+
+  const handleRemoveCurrency = (currency: string) => {
+    dispatch(removeCurrency(currency));
+  };
+
+  const handleBaseCurrencyChange = (currency: string) => {
+    dispatch(setBaseCurrency(currency));
+  };
+
+  useEffect(() => {
+    dispatch(loadAvailableCurrencies());
+  }, [dispatch]);
 
   return (
     <Container maxWidth="lg" sx={{ py: 4 }}>
@@ -18,7 +54,27 @@ export const ExchangeRatesPage = () => {
       >
         View exchange rates for the last 7 days from your selected date
       </Typography>
-      <DatePicker selectedDate={selectedDate} onDateChange={setSelectedDate} />
+      <Box mb={3}>
+        <DatePicker
+          selectedDate={selectedDate}
+          onDateChange={setSelectedDate}
+        />
+      </Box>
+      <Box mb={3}>
+        <CurrencySelector
+          baseCurrency={baseCurrency}
+          selectedCurrencies={selectedCurrencies}
+          availableCurrencies={availableCurrencies}
+          onBaseCurrencyChange={handleBaseCurrencyChange}
+          onAddCurrency={handleAddCurrency}
+          onRemoveCurrency={handleRemoveCurrency}
+        />
+      </Box>
+      <CurrencyTable
+        exchangeRates={exchangeRates}
+        selectedCurrencies={selectedCurrencies}
+        loading={loading}
+      />
     </Container>
   );
 };
